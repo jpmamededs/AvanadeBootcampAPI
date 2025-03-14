d379f014416d1d4807c39d883f218ebb6eb14284
João Pedro Mamede <joaopedrodias@live.com>
2025-03-14 14:35:55 -0300

    Init commit

---
+++ src/main/java/mamede/api_development/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package mamede.api_development.domain.repository;
+
+import mamede.api_development.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ src/test/java/mamede/api_development/ApiDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package mamede.api_development;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
